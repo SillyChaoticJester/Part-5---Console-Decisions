935c9f248c398bda44ca89101b8be8ef244f2dbd
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-14 14:02:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Part_5___Console_Decisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Notes
+            //Symbols for If/Else If/Else Statements:
+            //
+            // == - Equals
+            // < - Less Than
+            // > - Greater Than
+            // <= - Less Than or Equal To
+            // >= - Greater Than or Equal To
+            // != - Not Equal To
+            // && - And
+            // || - Or
+            // ! - Not
+
+            // If Statements only work if a condition is true
+            // Else If Statements only work if the previous If Statement was false, but its own condition was true
+            // Else Statements only work if none of the following conditions were true
+        }
+    }
+}
